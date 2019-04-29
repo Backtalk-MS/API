57f62af108fdfff89e29d59f7d9e047549cd75cd
alexman258 <alexman258@gmail.com>
2019-04-29 10:15:00 -0700

    Added stubs for model training functions

--- src/server.py
+++ src/server.py
@@ -31,5 +31,27 @@ def predict():
     # Return output
     return jsonify(sentiment)
 
+
+"""Generic model trains on a new dataset. Model ID is stored in the Database
+This model can then be used for prediction purposes in the future""" 
+
+@app.route('/train_generic', methods=['POST'])
+def train_generic():
+    #TODO: Request JSON containing all info such as link to Database that will contain the dataset
+    # Also, needs a "labels" and "categories" from the categories so model knows which items to train on
+    return
+
+
+"""New model trains on a new dataset. Model ID is stored in the Database
+This model can then be used for prediction purposes in the future""" 
+@app.route('/train', methods=['POST'])
+def train():
+    #TODO: Request JSON containing all info such as link to Database that will contain the dataset
+    # Also, needs a "labels" and "categories" from the categories so model knows which items to train on
+    # Also, json input will contain the format of a model. (Look at keras function save_json() for examples on this format. 
+    # Keras can use this to construct a model from JSON rather than from code. Model.fit() function can then be run in order
+    # to train this model. The .h5 file model weights, and .JSON model structure are both saved to the DB after training.
+    return
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
